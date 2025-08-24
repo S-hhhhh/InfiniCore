4d68243d644c9b564d67383c7edd54f97109bf84
S-hhhhh <2320230838@mail.nankai.edu.cn>
2025-08-24 22:46:52 +0800

    fix: windows build

--- src/infiniop/ops/reduce_max/cpu/reduce_max_cpu.cc
+++ src/infiniop/ops/reduce_max/cpu/reduce_max_cpu.cc
@@ -20,11 +20,7 @@ infiniStatus_t Descriptor::create(
 
 template <typename T>
 infiniStatus_t reduce_max(const ReduceMaxInfo *info, T *y, const T *x) {
-    const size_t batch_size = info->shape[0];
-    const size_t channels = info->shape[1];
-    const size_t rows = info->shape[2];
     const size_t cols = info->shape[3]; // 规约维度
-
     const ptrdiff_t y_batch_stride = info->y_strides[0];
     const ptrdiff_t y_channel_stride = info->y_strides[1];
     const ptrdiff_t y_row_stride = info->y_strides[2];
@@ -32,7 +28,35 @@ infiniStatus_t reduce_max(const ReduceMaxInfo *info, T *y, const T *x) {
     const ptrdiff_t x_channel_stride = info->x_strides[1];
     const ptrdiff_t x_row_stride = info->x_strides[2];
     const ptrdiff_t x_col_stride = info->x_strides[3];
+#ifdef _WIN32
+    const ptrdiff_t batch_size = static_cast<ptrdiff_t>(info->shape[0]);
+    const ptrdiff_t channels = static_cast<ptrdiff_t>(info->shape[1]);
+    const ptrdiff_t rows = static_cast<ptrdiff_t>(info->shape[2]);
+#pragma omp parallel for
+    for (ptrdiff_t batch = 0; batch < batch_size; ++batch) {
+        for (ptrdiff_t channel = 0; channel < channels; ++channel) {
+            for (ptrdiff_t row = 0; row < rows; ++row) {
+                const T *input_start = x + batch * x_batch_stride
+                                     + channel * x_channel_stride
+                                     + row * x_row_stride;
+                T *output_ptr = y + batch * y_batch_stride
+                              + channel * y_channel_stride
+                              + row * y_row_stride;
 
+                float max = op::common_cpu::reduce_op::max(input_start, cols, x_col_stride);
+
+                if constexpr (std::is_same<T, fp16_t>::value || std::is_same<T, bf16_t>::value) {
+                    *output_ptr = utils::cast<T>(max);
+                } else {
+                    *output_ptr = max;
+                }
+            }
+        }
+    }
+#else
+    const size_t batch_size = info->shape[0];
+    const size_t channels = info->shape[1];
+    const size_t rows = info->shape[2];
 #pragma omp parallel for collapse(3)
     for (size_t batch = 0; batch < batch_size; ++batch) {
         for (size_t channel = 0; channel < channels; ++channel) {
@@ -54,7 +78,7 @@ infiniStatus_t reduce_max(const ReduceMaxInfo *info, T *y, const T *x) {
             }
         }
     }
-
+#endif //_WIN32
     return INFINI_STATUS_SUCCESS;
 }
 infiniStatus_t Descriptor::calculate(
